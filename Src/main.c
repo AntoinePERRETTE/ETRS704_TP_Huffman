ee95d69777443f0c238c2afb7f125f09204efcfc
Antoine PERRETTE <antoineperrette71@ik.me>
2025-10-30 15:49:29 +0100

    Compression algorythm fully implemented

--- Src/main.c
+++ Src/main.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
 #define TAILLE_MAX_COMPRESS 500
 
@@ -37,7 +38,7 @@ void afficherTabArbreHuffman(struct noeud* arbre[256], uint32_t taille) {
     printf("\n[DEBUG] -> Affichage des noeuds générés:\r\n");
     for(uint32_t i = 0; i < taille; i++) {
         if (arbre[i] != NULL) {
-            printf("noeud %d : c = %c, occurence = %d, code = %X, tailleCode = %d\r\n", i, arbre[i]->c, arbre[i]->occurence, arbre[i]->code, arbre[i]->tailleCode);
+            printf("noeud %d : c = %c, occurence = %d, code = %b, tailleCode = %d\r\n", i, arbre[i]->c, arbre[i]->occurence, arbre[i]->code, arbre[i]->tailleCode);
         } else {
             printf("noeud %d n'existe pas\r\n", i);
         }
@@ -46,9 +47,9 @@ void afficherTabArbreHuffman(struct noeud* arbre[256], uint32_t taille) {
 
 void afficherArbre(struct noeud * ptrNoeud) {
 	if(ptrNoeud->droite == NULL && ptrNoeud->gauche == NULL) {
-		printf("->[N : %c ; Occ : %d ; code = %X]\0338\n", ptrNoeud->c, ptrNoeud->occurence, ptrNoeud->code);
+		printf("->[N : %c ; Occ : %d ; code = %b]\0338\n", ptrNoeud->c, ptrNoeud->occurence, ptrNoeud->code);
 	} else {
-		printf("->[N : %c ; Occ : %d ; code = %X]\0337", ptrNoeud->c, ptrNoeud->occurence, ptrNoeud->code);
+		printf("->[N : %c ; Occ : %d ; code = %b]\0337", ptrNoeud->c, ptrNoeud->occurence, ptrNoeud->code);
 		afficherArbre(ptrNoeud->droite);
 		afficherArbre(ptrNoeud->gauche);
 	}
@@ -57,7 +58,7 @@ void afficherArbre(struct noeud * ptrNoeud) {
 uint8_t creerFeuille(struct noeud * arbre[256], uint32_t tab[256]) {
 	uint8_t j = 0;
     struct noeud * ptrNoeud = NULL;
-    for(uint8_t i = 0; i < 255; i++) {
+    for(uint16_t i = 0; i < 256; i++) {
         if (tab[i] != 0) {
             ptrNoeud = (struct noeud *) malloc(sizeof(struct noeud));
 
@@ -99,12 +100,12 @@ void creerCode(struct noeud * ptrNoeud, uint32_t code, uint32_t taille) {
 	if(ptrNoeud->droite == NULL && ptrNoeud->gauche == NULL) {
 		ptrNoeud->tailleCode = taille;
 		ptrNoeud->code = code;
-		printf("%c \t code : %d \t taille : %d \r\n", ptrNoeud->c, ptrNoeud->code, ptrNoeud->tailleCode);
+		printf("%c \t code : %b \t taille : %d \r\n", ptrNoeud->c, ptrNoeud->code, ptrNoeud->tailleCode);
 	} else {
-		//On va a droite (on injecte un 0 à droite dans le code)
-		creerCode(ptrNoeud->droite, code << 1, taille + 1);
-		//On va a gauche (on injecte un 1 à droite)
-		creerCode(ptrNoeud->gauche, (code << 1) + 1, taille + 1);
+		//On va a gauche (on injecte un 0 à droite dans le code)
+		creerCode(ptrNoeud->gauche, code << 1, taille + 1);
+		//On va a droite (on injecte un 1 à droite)
+		creerCode(ptrNoeud->droite, (code << 1) + 1, taille + 1);
 	}
 }
 
@@ -122,39 +123,129 @@ void triArbre(struct noeud * arbre[256], uint32_t taille) {
 	}
 }
 
-struct noeud * getAddress(struct noeud * ptrNoeud, uint8_t caractere) {
-	if(ptrNoeud->c == caractere) return ptrNoeud;
-	else if(ptrNoeud->droite != NULL) return getAddress(ptrNoeud->droite, caractere);
-	else if(ptrNoeud->gauche != NULL) return getAddress(ptrNoeud->gauche, caractere);
+struct noeud * getAddress(struct noeud * racine, uint8_t caractere) {
+    struct noeud * ptrNoeud = NULL;
+	if(racine->c == caractere) {
+        return racine;
+    } else if(racine->droite != NULL && (ptrNoeud = getAddress(racine->droite, caractere)) != NULL) {
+        return ptrNoeud;
+    } else if(racine->gauche != NULL && (ptrNoeud = getAddress(racine->gauche, caractere)) != NULL) {
+        return ptrNoeud;
+    }
+    return NULL;
+}
+
+void afficherCaractereEtCode(struct noeud * racine, uint8_t * texte) {
+    struct noeud * ptrNoeud = NULL;
+    while(*texte != 0) {
+        ptrNoeud = getAddress(racine, *texte);
+        if (ptrNoeud != NULL) printf("[DEBUG] -> Le caractere %d à pour code %b\r\n", (uint8_t) *texte, ptrNoeud->code);
+        else printf("\r\n[DEBUG] -> Le caractere %d n'est pas dans l'arbre", (uint8_t) *texte);
+        texte++;
+    }
+}
+
+uint32_t compresse(uint8_t * texte, uint8_t texteCompress[TAILLE_MAX_COMPRESS], struct noeud * racine) {
+    struct noeud * ptrNoeud = NULL;
+    uint8_t symbole = 0;
+    uint8_t tailleSymbole = 0;
+    uint32_t index = 0;
+    uint32_t tailleRestante = 0;
+    while(*texte != 0 && index < TAILLE_MAX_COMPRESS) {
+        ptrNoeud = getAddress(racine, *texte);
+
+        tailleRestante = 8 - (uint32_t) tailleSymbole;
+
+        if(ptrNoeud->tailleCode <= tailleRestante) {
+            symbole <<= ptrNoeud->tailleCode;
+            symbole |= (uint8_t) ptrNoeud->code;
+            tailleSymbole += ptrNoeud->tailleCode;
+        } else {
+            symbole <<= tailleRestante;
+            symbole |= (uint8_t) ptrNoeud->code >> (-tailleRestante - ptrNoeud->tailleCode);
+
+            texteCompress[index] = symbole;
+            tailleSymbole = 0;
+            symbole = 0;
+            index++;
+
+            symbole |= (uint8_t) ptrNoeud->code;
+            tailleSymbole += ptrNoeud->tailleCode - tailleRestante;
+        }
+
+        texte++;
+    }
+    symbole <<= 8 - tailleSymbole;
+    texteCompress[index] = symbole;
+
+    return index+1;
+}
+
+void creerFichier(uint8_t fichier[], uint8_t texteCompress[], struct noeud * racine, uint16_t tailleFichierCompresse, uint16_t nbrCaractereTotal, uint32_t tab[256]) {
+   fichier[2] = tailleFichierCompresse & 0x0F;
+   fichier[3] = tailleFichierCompresse & 0xF0;
+
+   fichier[4] = nbrCaractereTotal & 0x0F;
+   fichier[5] = nbrCaractereTotal & 0xF0;
+
+   uint16_t index = 6;
+   struct noeud * ptrNoeud = NULL;
+
+   for(uint16_t i = 0; i < 256; i++) {
+       if(tab[i] != 0) {
+           ptrNoeud = getAddress(racine, i);
+           fichier[index] = i;
+
+           fichier[++index] = ptrNoeud->code & 0x000F;
+           fichier[++index] = ptrNoeud->code & 0x00F0;
+           fichier[++index] = ptrNoeud->code & 0x0F00;
+           fichier[++index] = ptrNoeud->code & 0xF000;
+
+           fichier[++index] = ptrNoeud->tailleCode & 0x000F;
+           fichier[++index] = ptrNoeud->tailleCode & 0x00F0;
+           fichier[++index] = ptrNoeud->tailleCode & 0x0F00;
+           fichier[++index] = ptrNoeud->tailleCode & 0xF000;
+
+           index++;
+       }
+   }
+   fichier[index] = 2;  //Start of Text
+   index++;
+
+   for(uint16_t i = index; (i - index) < tailleFichierCompresse; i++) {
+       fichier[i] = texteCompress[i - index];
+   }
+   index += tailleFichierCompresse;
+
+   fichier[0] = index & 0x0F;
+   fichier[1] = index & 0xF0;
 }
 
 int main(void)
 {
-	GPIO_Init();
-	SYSTICK_Init();
-	USART2_Init();
-
 	// Texte non compressé
-	uint8_t texte[] = "aaaabbbccd";
-	//uint8_t texte[] = "Une banane";
+	// uint8_t texte[] = "aaaabbbccd";
+	uint8_t texte[] = "Une banane";
 
 	// Texte compressé
-	//uint8_t texteCompress[TAILLE_MAX_COMPRESS];
+	uint8_t texteCompress[TAILLE_MAX_COMPRESS] = {0};
 
 	// Tableau du nombre d'occurence de chaque caractère
 	uint32_t tabCaractere[256] = {0};
 
 	// Nombre de caractère total dans le texte non compréssé
-	//uint32_t nbrCaractereTotal = 0;
+	uint16_t nbrCaractereTotal = strlen((char *) texte);
 
-	// Nombre de caractère différent dans le texte non compréssé
-	//uint32_t nbrCaractereDifferent = 0;
+    // Nombre de caractère total dans le texte compréssé
+    uint16_t nbrCaractereCompresseTotal = 0;
 
 	// Nombre de noeuds avec caractère
 	uint8_t nbrNoeuds = 0;
 
 	// Arbre de Huffman
 	struct noeud* arbreHuffman[256] = {NULL};
+
+    uint8_t fichierFinal[1000] = {0};
 
 	printf("\r\n\n[DEBUG] -> Calcul des occurences\r\n");
 	occurence(texte, tabCaractere);
@@ -185,5 +276,18 @@ int main(void)
 	printf("\n[DEBUG] -> Affichage de l'arbre\r\n");
 	afficherArbre(racine);
 
-	struct noeud * n = getAddress(racine, 'c');
+	//struct noeud * n = getAddress(racine, 'c');
+    afficherCaractereEtCode(racine, texte);
+
+    nbrCaractereCompresseTotal = compresse(texte, texteCompress, racine);
+
+    for(uint32_t i = 0; i <= nbrCaractereCompresseTotal; i++) {
+        printf("%b\r\n", texteCompress[i]);
+    }
+
+    creerFichier(fichierFinal, texteCompress, racine, nbrCaractereCompresseTotal, nbrCaractereTotal, tabCaractere);
+
+    for(uint16_t i = 0; i < TAILLE_MAX_COMPRESS; i++) {
+        printf("%X/", fichierFinal[i]);
+    }
 }
