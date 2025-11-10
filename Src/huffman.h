5dea3064117a768d560f41e5155ce7abbbe90f6c
Antoine PERRETTE <antoineperrette71@ik.me>
2025-11-10 16:18:09 +0100

    Define Huffman tree node structure and constants
    Added a new structure for Huffman tree nodes and defined constants.

--- Src/huffman.h
+++ Src/huffman.h
@@ -1,5 +1,18 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
+
+
+#define TAILLE_MAX_COMPRESS 500
+
+// Noeud de l'Arbre
+struct noeud{
+    uint8_t c;                      // Caractère initial
+    uint32_t occurence;             // nombre d'occurrences
+    uint32_t code;                  // Code binaires dans l'arbre
+    uint32_t tailleCode;            // Nombre de bits du code
+    struct noeud *gauche, *droite;  // Lien vers les noeuds suivants
+};
+
 void occurence(uint8_t* chaine, uint32_t tab[]);
 
 uint8_t creerFeuille(struct noeud * arbre[256], uint32_t tab[256]);
